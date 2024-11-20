971a482bd247fbaf6f3424b9e25666a12e5e8e22
Qwyn-Petersen <qwin@pdx.edu>
2024-11-20 12:44:06 -0800

    This commit completely rewrites over the previous code properly handling the data in a simple way (code came from an office hour session with Dr. Rodriguez)

--- Offline_data_R.R
+++ Offline_data_R.R
@@ -1,19 +1,35 @@
-data_1<-readLines("C:/Users/risin/OneDrive/Desktop/offline.final.trace.txt", n = 913)
-data_1<- data_1[-(1:3)]
-head(data_1)
-library(stringr)
+# sets the current working directory
+setwd("C:/Users/risin/OneDrive/Desktop/School/Classes/STAT410/IPS_data")
+# Loads the offline.final.trace.txt into an object
+txt <- readLines("offline.final.trace.txt")
 
-break_1<-str_split(data_1[1],pattern = ";")
-key_info<-lapply(str_split(break_1[[1]][1:4],"="),"[[",2)
-names<-lapply(str_split(break_1[[1]][1:4],"="),"[[",1)
-n=4
-X<-matrix(key_info,ncol=n,nrow=length(break_1[[1]])-n,byrow=T)
-colnames(X) <- names
-X<-cbind(X,break_1[[1]][-(1:4)])
+# Txt is a character vector of length 151392
+str(txt)
+# There are 5312 comments in the data
+sum(substr(txt,1,1) =="#")
 
-library(tidyr)
-df_1 <- as.data.frame(X, stringsAsFactors = FALSE)
-df_1$V5 <- as.character(df_1$V5)
-df_1.1 <- separate(df_1, col = "V5", into = c("Router", "Signal_Strength"), sep = "=")
-df_1.2 <- separate(df_1.1, col = "Signal_Strength", into = c("Signal_Strength","Frequency","Mode"), sep = ",")
+# This function splits the text line to match specific columns names its respective row of data
+processLine <- function(x){
+  # Regex in the strsplit function to split data on ;=, characeters (from looking at the data)
+  tokens = strsplit(x,"[;=,]")[[1]]
+  # Handles the case where there are no recorded signals (not useful for this project) and removes the data                           
+  if (length(tokens) == 10){
+    return(NULL)
+  }
+  # For each signal recording, tokens 1,3,5, and 9 are columns names
+  tmp = matrix(tokens[-(1:10)],ncol=4, byrow=T)
+  cbind(matrix(tokens[c(2,4,6:8,10)],nrow=nrow(tmp),ncol=6,byrow=T),tmp)
+}
 
+# Runs the porcessLine function over the entire dataset to build dataframe
+lines <- txt[substr(txt,1,1) != "#"] # Removes comments from data
+tmp <- lapply(lines,processLine) 
+train <- as.data.frame(do.call("rbind",tmp), stringAsFactors=F)
+
+# Check dimensions of training set
+dim(train)
+head(train)
+str(train)
+
+# Assigns names to each of the columns
+names(train) <- c("time","scanMac","posX","posY","posZ","orientation","mac","signal","channel","type")
