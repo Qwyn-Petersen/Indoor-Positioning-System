e0f853e9c57bb97a0fe7ae1be7d39db991ffdd92
Qwyn-Petersen <qwin@pdx.edu>
2024-12-01 12:01:14 -0800

    This commit loads and gives basic format to the online data (testing data)

--- Offline_data_R.R
+++ Offline_data_R.R
@@ -1,12 +1,14 @@
 # sets the current working directory
 setwd("C:/Users/risin/OneDrive/Desktop/School/Classes/STAT410/IPS_data")
-# Loads the offline.final.trace.txt into an object
-txt <- readLines("offline.final.trace.txt")
 
-# Txt is a character vector of length 151392
-str(txt)
+# Loads the offline.final.trace.txt into an object
+offline_data <- readLines("offline.final.trace.txt")
+
+# offline_data is a character vector of length 151392
+str(offline_data)
+
 # There are 5312 comments in the data
-sum(substr(txt,1,1) =="#")
+sum(substr(offline_data,1,1) =="#")
 
 # This function splits the text line to match specific columns names its respective row of data
 processLine <- function(x){
@@ -21,8 +23,8 @@ processLine <- function(x){
   cbind(matrix(tokens[c(2,4,6:8,10)],nrow=nrow(tmp),ncol=6,byrow=T),tmp)
 }
 
-# Runs the porcessLine function over the entire dataset to build dataframe
-lines <- txt[substr(txt,1,1) != "#"] # Removes comments from data
+# Runs the porcessLine function over the entire data set to build a data frame
+lines <- offline_data[substr(offline_data,1,1) != "#"] # Removes comments from data
 tmp <- lapply(lines,processLine) 
 train <- as.data.frame(do.call("rbind",tmp), stringAsFactors=F)
 
@@ -33,3 +35,34 @@ str(train)
 
 # Assigns names to each of the columns
 names(train) <- c("time","scanMac","posX","posY","posZ","orientation","mac","signal","channel","type")
+
+### MODIFY THE DATA FRAME AS NECESSARY
+####
+#####
+
+# Loads the online.final.trace.txt into an object
+online_data <- readLines("online.final.trace.txt")
+
+# online_data is a character vector of length 6832
+str(online_data)
+
+# There are 240 comments in the data
+sum(substr(online_data,1,1) =="#")
+
+# Runs the porcessLine function over the entire data set to build a data frame
+lines <- online_data[substr(online_data,1,1) != "#"] # Removes comments from data
+tmp <- lapply(lines,processLine) 
+test <- as.data.frame(do.call("rbind",tmp), stringAsFactors=F)
+
+# Check dimensions of training set
+dim(test)
+head(test)
+str(test)
+
+# Assigns names to each of the columns
+names(test) <- c("time","scanMac","posX","posY","posZ","orientation","mac","signal","channel","type")
+
+
+
+
+
